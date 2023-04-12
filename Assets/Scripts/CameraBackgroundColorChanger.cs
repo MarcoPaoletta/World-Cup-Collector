d44e9c245297c237718562ae0a12d1aa26825b47
Marco Paoletta <marcopaoletta2007@gmail.com>
2023-04-12 16:14:30 -0300

    Change camera bg color depending on platform

---
+++ Assets/Scripts/CameraBackgroundColorChanger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBackgroundColorChanger : MonoBehaviour
+{
+    [SerializeField] private Color lightBlueColor;
+
+    private void Start()
+    {
+        if(Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            Camera.main.backgroundColor = Color.white;
+        }
+        else
+        {
+            Camera.main.backgroundColor = lightBlueColor;
+        }
+    }
+}
